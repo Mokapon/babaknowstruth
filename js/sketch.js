02a8800397b13dae765b648f229e090da335d626
Cecile Camillieri <cecile.camillieri@gmail.com>
2018-11-04 13:02:37 +0100

    Limit size of the window, add words 'not', 'and' and 'on' and allow longer sentences.

--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,35 @@
-let TILE_WIDTH = 24;
-let IS_CHANCE = 0.3;
+let MIN_SENTENCE_LENGTH = 3;
+
+let TILE_SIZE = 24;
+let MIN_WIDTH = TILE_SIZE * (MIN_SENTENCE_LENGTH+2);
+let MIN_HEIGHT = MIN_WIDTH;
+
+let CHANCE_IS = 0.3;
+let CHANCE_NEGATION = 0.2;
+let CHANCE_CONNECTION = 0.2;
 
 let backColor = '#080808';
 let accentColor = '#de396b';
-let colors = [ '#efe784', '#e75239', '#8c5a9c', '#5a9cd6' ];
+let connectorColor = '#fff';
+let negationColor = '#c65a9c'
+let decorColors = [ '#efe784', '#e75239', '#8c5a9c', '#5a9cd6' ];
 
-let nouns = ['all', 'baba', 'cute', 'fruit', 'ghost', 'keke', 'life', 'love', 'metal', 'poof', 'win', 'w_star', 'you' ];
-let verbs = ['is', 'make', 'eat', 'heart', 'follow'];
-let decor = ['dust', 'star'];
+let nouns = ['all', 'baba', 'cute', 'ghost', 'keke', 'life', 'love', 'metal', 'poof', 'win', 'w_star', 'you' ];
+let verbs = ['is', 'make', 'eat', 'follow'];
+let connectors = ['and', 'on'];
+let negation = ['not'];
+let decor = ['dust', 'star', 'heart'];
 
 let images = [];
 
 let truth = [];
 let sparkles = [];
 
-let w, h, truthCol=1, truthRow=1, scale=1;
+let scale=1;
+let truthCol=1, truthRow=1;
 
 function preload() {
-    for (let word of concat(decor, concat(nouns, verbs))) {
+    for (let word of concat(connectors, concat(negation, concat(decor, concat(nouns, verbs))))) {
         images[word] = [];
         images[word].push(loadImage('img/'+word+'1.png'));
         images[word].push(loadImage('img/'+word+'2.png'));
@@ -25,9 +37,11 @@ function preload() {
 }
 
 function setup() {
+
   let canvas = createCanvas(windowWidth, windowHeight - 4);
   canvas.parent('canvas-holder')
-  // create canvas and disable smoothing
+
+  // disable smoothing
   let context = canvas.elt.getContext('2d');
   context.mozImageSmoothingEnabled = false;
   context.webkitImageSmoothingEnabled = false;
@@ -36,21 +50,15 @@ function setup() {
 
   // Handle the info modal
   let modal = document.getElementById('infobox');
-  
   let openBtn = document.getElementById('button-open');
   let closeBtn = document.getElementById('button-close');
 
-  // When the user clicks the button, open the modal 
   openBtn.onclick = function() {
     modal.style.display = 'block';
   }
-
-  // When the user clicks on <span> (x), close the modal
   closeBtn.onclick = function() {
     modal.style.display = 'none';
   }
-
-  // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target != modal && event.target != openBtn) {
       modal.style.display = 'none';
@@ -59,69 +67,108 @@ function setup() {
 
   // compute location of the different elements and display
   windowResized();
-
-  noStroke();
-
 }
 
 function windowResized() {
-  w = windowWidth;
-  // I don't know OK? p5 adds 4 pixels to the container and it's annoying me
-  h = windowHeight -40;
+  let w = max(windowWidth, (truth.length+2)*TILE_SIZE, MIN_WIDTH);
+  // I don't know OK? p5 adds a few pixels to the canvas container and it's annoying me
+  let h = max(windowHeight -8, MIN_HEIGHT);
+  
+  if (w !== width || h !== height) {
+    setCanvasSize(w,h);
+  }
+}
+
+function setCanvasSize(w,h) {
   resizeCanvas(w, h);
 
   let prevScale = scale;
   let prevTruthCol = truthCol;
   let prevTruthRow = truthRow;
 
-  scale = ceil(max(1,min(w/TILE_WIDTH/9, h/TILE_WIDTH/9)));
+  scale = ceil(max(1,min(w/TILE_SIZE/9, h/TILE_SIZE/9)));
 
-  let numCols = floor(w/TILE_WIDTH/scale);
-  let numRows = floor(h/TILE_WIDTH/scale);
+  let numCols = floor(w/TILE_SIZE/scale);
+  let numRows = floor(h/TILE_SIZE/scale);
 
-  truthCol = floor(numCols/2-1);
+  while((truth.length+2)>numCols) {
+    scale--;
+    numCols = floor(w/TILE_SIZE/scale);
+    numRows = floor(h/TILE_SIZE/scale);
+  }
+
+  // pick sentence if it's the first time
+  if (!truth || truth.length==0) {
+    pickTruth(numCols-2);
+  }
+
+  // Place the sentence in the right position
+  truthCol = floor(numCols/2 - truth.length/2);
   truthRow = floor(numRows/2);
+  for (let i=0; i < truth.length;i++) {
+    truth[i].setPosition(truthCol + i, truthRow);
+  }
   
   // decorative elements
   if (!sparkles || sparkles.length==0 
     || scale != prevScale
     || prevTruthCol != truthCol || prevTruthRow != truthRow) {
-    let col, row;
-    sparkles=[];
-    for (let i=0;i<random(10)+(numCols*numRows)/10;i++) {
-      col = truthCol;
-      row = truthRow;
-      while (col >=truthCol-1 && col <= truthCol+3 &&
-        row >=truthRow-1 && row <= truthRow+1) {
-        col = floor(random(numCols));
-        row = floor(random(numRows))
-      }
-      sparkles.push(new Sprite(col,row,random(colors), images[random(decor)]));
-    }
-  }
-
-  // pick words if necessary
-  if (!truth || truth.length==0) {
-    pickTruth(truthCol, truthRow);
-  } else {
-    for (let i=0;i<truth.length;i++) {
-      truth[i].x = truthCol + i;
-      truth[i].y = truthRow;
-    }
+    createDecorElements(numCols, numRows);
   }
 
   drawTruth();
 }
 
-function pickTruth(col, row) {
-  truth = [];
-  truth.push(new Sprite(col, row, accentColor, images[random(nouns)]));
-  let verb = 'is';
-  if (random() > IS_CHANCE) {
-    verb = random(verbs);
+function createDecorElements(numCols, numRows) {
+  let col, row;
+  sparkles=[];
+  for (let i=0;i<random(10)+(numCols*numRows)/10;i++) {
+    col = truthCol;
+    row = truthRow;
+    while (col >=truthCol-1 && col <= truthCol+truth.length &&
+      row >=truthRow-1 && row <= truthRow+1) {
+      col = floor(random(numCols));
+      row = floor(random(numRows))
+    }
+    sparkles.push(new Sprite(random(decorColors), images[random(decor)],col,row));
   }
-  truth.push(new Sprite(col+1, row, '#fff', images[verb]));
-  truth.push(new Sprite(col+2, row, accentColor, images[random(nouns)]));
+}
+
+function pickTruth(maxLength) {
+  truth = [];
+  let possibleGroups = floor((maxLength-1) / 2);
+  let groups = 1;
+
+  for (let i=1; i < possibleGroups; i++) {
+    if (random() < CHANCE_CONNECTION) {
+      groups++;
+    }
+  }
+
+  let sizeLeft = maxLength - (groups*2 +1);
+
+  sizeLeft -= pickNoun(sizeLeft>0);
+  for (let i = 0; i <groups; i++) {
+    pickConnector();
+    sizeLeft -= pickNoun(sizeLeft>0);
+  }
+}
+
+function pickNoun(canNegate) {
+  let negate = canNegate && random() < CHANCE_NEGATION
+  if (negate) {
+    truth.push(new Sprite(negationColor, images[random(negation)]));
+  }
+  truth.push(new Sprite(accentColor, images[random(nouns)]));
+  return negate ? 1 : 0;
+}
+
+function pickConnector() {
+  let connector = 'is';
+  if (random() > CHANCE_IS) {
+    connector = random(concat(verbs, connectors));
+  }
+  truth.push(new Sprite(connectorColor, images[connector]));
 }
 
 function draw() {
--- js/sprite.js
+++ js/sprite.js
@@ -1,9 +1,14 @@
-function Sprite(x, y, col, images) {
+function Sprite(col, images, x, y) {
     this.x=x;
     this.y=y;
     this.col=col;
     this.images=images || [];
     this.currentImage = 0;
+
+    this.setPosition = function(x, y) {
+        this.x = x
+        this.y = y;
+    }
 
     this.tick = function() {
         this.currentImage = (this.currentImage+1)%this.images.length;
@@ -11,6 +16,6 @@ function Sprite(x, y, col, images) {
 
     this.draw = function() {
         tint(this.col);
-        image(this.images[this.currentImage], this.x*TILE_WIDTH*scale, this.y*TILE_WIDTH*scale,TILE_WIDTH*scale,TILE_WIDTH*scale);
+        image(this.images[this.currentImage], this.x*TILE_SIZE*scale, this.y*TILE_SIZE*scale,TILE_SIZE*scale,TILE_SIZE*scale);
     }
 }
